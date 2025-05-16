50de158f1f7903954e2bcd97134d094ebb7bca50
rolevax <rolevax@outlook.com>
2025-05-16 14:51:07 +0800

    add slave struct

--- src/SlaveQA.sol
+++ src/SlaveQA.sol
@@ -2,13 +2,47 @@
 pragma solidity ^0.8.28;
 
 contract SlaveQA {
-    uint256 public number;
-
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+    struct Slave {
+        address self;
+        string desc;
+        uint256 price;
+        address master;
+        address[] slaves;
     }
 
-    function increment() public {
-        number++;
+    mapping(address => Slave) addressToSlave;
+    address[] addresses;
+
+    function getOrDefault(address a) public view returns (Slave memory) {
+        if (a == address(0)) {
+            a = msg.sender;
+        }
+
+        Slave memory slave = addressToSlave[a];
+        if (slave.self == address(0)) {
+            slave.self = msg.sender;
+        }
+
+        return slave;
+    }
+
+    function sellSelf(uint256 price, string memory desc) public {
+        Slave storage slave = addressToSlave[msg.sender];
+        if (slave.self == address(0)) {
+            slave.self = msg.sender;
+            addresses.push(msg.sender);
+        }
+
+        slave.price = price;
+        slave.desc = desc;
+    }
+
+    function getSlaves() public view returns (Slave[] memory) {
+        uint256 len = addresses.length;
+        Slave[] memory results = new Slave[](len);
+        for (uint256 i = 0; i < len; i++) {
+            results[i] = addressToSlave[addresses[i]];
+        }
+        return results;
     }
 }
--- test/SlaveQA.t.sol
+++ test/SlaveQA.t.sol
@@ -5,20 +5,36 @@ import {Test, console} from "forge-std/Test.sol";
 import {SlaveQA} from "../src/SlaveQA.sol";
 
 contract SlaveQATest is Test {
-    SlaveQA public counter;
+    SlaveQA public slaveQA;
+
+    address USER1 = makeAddr("alice");
+    address USER2 = makeAddr("bob");
+    uint256 USER_BALANCE = 1 ether;
 
     function setUp() public {
-        counter = new SlaveQA();
-        counter.setNumber(0);
+        slaveQA = new SlaveQA();
+        deal(USER1, USER_BALANCE);
+        deal(USER2, USER_BALANCE);
     }
 
-    function test_Increment() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
+    function test_get() public {
+        vm.prank(USER1);
+
+        SlaveQA.Slave memory s = slaveQA.getOrDefault(address(0));
+        assertEq(s.self, USER1);
     }
 
-    function testFuzz_SetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+    function test_sellSelf() public {
+        vm.prank(USER1);
+        slaveQA.sellSelf(114514, "aaa");
+
+        SlaveQA.Slave memory s = slaveQA.getOrDefault(USER1);
+        assertEq(s.self, USER1);
+        assertEq(s.price, 114514);
+        assertEq(s.desc, "aaa");
+
+        SlaveQA.Slave[] memory slaves = slaveQA.getSlaves();
+        assertEq(slaves.length, 1);
+        assertEq(slaves[0].self, USER1);
     }
 }
